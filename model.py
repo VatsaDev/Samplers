583e496a2562c9dbec1f89cabdc7999fa2725e9d
VatsaDev <71975550+VatsaDev@users.noreply.github.com>
2024-03-01 22:26:02 -0600

    Update model.py

--- model.py
+++ model.py
@@ -310,15 +310,15 @@ class GPT(nn.Module):
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
         for _ in range(max_new_tokens):
-            print("n_tok")
+            #print("n_tok")
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
             # forward the model to get the logits for the index in the sequence
             logits, _ = self(idx_cond)
-            print(logits)
+            #print(logits)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
-            print(logits)
+            #print(logits)
 
             # pick between topk and topp
 
